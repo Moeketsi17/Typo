280caba5f757d548374d377e187b81279b0f7606
moeketsi <raymondkxng17@gmail.com>
2022-05-08 13:17:09 +0200

    commit

---
+++ index.js
@@ -0,0 +1,71 @@
+let time = 5;
+let score = 0;
+let isPlaying;
+
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
+
+const words = [
+    'generate',
+    'hat',
+    'density',
+    'purple',
+    'indian',
+    'freelancer',
+    'lithium',
+    'calendar',
+    'clock',
+    'equities',
+    'messsage',
+    'suburban',
+    'assuming',
+    'obstinance',
+    'play',
+    'pazazz',
+    'murder',
+    'fun',
+    'scandal',
+    'unique',
+    'tommorrow',
+    'yesterday',
+    'time',
+    'hero',
+    'establishment',
+    'revolver',
+    'laughter',
+    'space',
+    'javascript',
+    'joke',
+    'runway'
+];
+
+
+
+// initialize game
+
+function init() {
+    console.log(init)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
